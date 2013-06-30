f858e0e0267c352ef70bcf504a1abe131b7cd154
Dominik Goltermann <dominik@goltermann.cc>
2013-06-30 15:17:57 +0200

    response time now logging to database

--- lib/rs_benchmark.rb
+++ lib/rs_benchmark.rb
@@ -1,7 +1,4 @@
 require "rs_benchmark/version"
-require "rs_benchmark/response_time"
 require "rs_benchmark/logger"
-
 module RsBenchmark
-  # Your code goes here...
 end
--- lib/rs_benchmark/response_time.rb
+++ lib/rs_benchmark/response_time.rb
@@ -1,57 +1,41 @@
 require "rubyperf"
+require "benchmark"
 
 module RsBenchmark
   class ResponseTime
-    def initialize options
-      throw ArgumentError.new "Missing :group_name option" unless options[:group_name]
-      throw ArgumentError.new "Missing :log_folder option" unless options[:log_folder]
-      @options = options
-      @results = Hash.new
-      @semaphore = Mutex.new
 
-      @rubyperf = Perf::Meter.new
+    class RsBenchmarkResponseTime
+      include Mongoid::Document
+
+      field :tag, :type => String
+      field :data, :type => Hash
+
+      index :tag => 1
+      index "data.time" => 1
+
+      attr_accessible :tag, :data
     end
 
-    def benchmark tag, &block
-      time_start = Time.now
-      @rubyperf.measure tag do
+    def self.benchmark tag, &block
+      # if not in benchmark environment simply call block and exit
+      if ENV["RAILS_ENV"] != "benchmark"
+        block.call
+        return
+      end
+
+      bm = Benchmark.measure do
         block.call
       end
-      time_end = Time.now
-      duration = time_end-time_start
 
-      @semaphore.synchronize {
-        @results[tag] = [] unless @results.has_key? tag
-        @results[tag] << duration
-      }
-    end
-
-    def flush
-      @semaphore.synchronize {
-        @results.each do |tag, values|
-          values.each do |value|
-            file_handler.puts("#{tag};#{value*1000}")
-          end
-        end
-
-        @results = {}
-      }
-    end
-
-    def get_report
-      @rubyperf.report_simple
-    end
-
-    def file_handler
-      return @file_handler if @file_handler
-
-      file_path =  @options[:log_folder]+"/#{@options[:group_name]}/results.csv"
-      FileUtils.mkdir_p File.dirname(file_path)
-      @file_handler = File.open(file_path,"w")
-
-      @file_handler.puts "Tag;Real Time"
-
-      @file_handler
+      RsBenchmarkResponseTime.create!(
+        :tag => tag,
+        :data => {
+          :time => Time.now,
+          :real => bm.real,
+          :stime => bm.stime,
+          :utime => bm.utime
+        }
+      )
     end
   end
 end
